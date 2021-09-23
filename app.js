f529b86334fb15e8231f5f802da3d9af08826200
KorNito <kornelijuspacevicius@gmail.com>
2021-09-23 13:41:13 +0300

    input validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const toDoInput = document.querySelector(".todo-input");
 const toDoButton = document.querySelector(".todo-button");
 const toDoList = document.querySelector(".todo-list");
+const inputErrorMessage = document.querySelector(".error");
 
 toDoButton.addEventListener("click", addTodo);
 toDoList.addEventListener("click", handleToDoAction);
@@ -8,27 +9,31 @@ toDoList.addEventListener("click", handleToDoAction);
 function addTodo(event) {
   event.preventDefault();
 
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo-div");
+  if (toDoInput.value.trim() === "") {
+    showErrorMessage();
+  } else {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo-div");
 
-  const newTodo = document.createElement("li");
-  newTodo.innerText = toDoInput.value;
-  newTodo.classList.add("todo-li");
-  todoDiv.appendChild(newTodo);
+    const newTodo = document.createElement("li");
+    newTodo.innerText = toDoInput.value;
+    newTodo.classList.add("todo-li");
+    todoDiv.appendChild(newTodo);
 
-  const markButton = document.createElement("button");
-  markButton.innerText = "Mark";
-  markButton.classList.add("mark-button");
-  todoDiv.appendChild(markButton);
+    const markButton = document.createElement("button");
+    markButton.innerText = "Mark";
+    markButton.classList.add("mark-button");
+    todoDiv.appendChild(markButton);
 
-  const removeButton = document.createElement("button");
-  removeButton.innerText = "Remove";
-  removeButton.classList.add("remove-button");
-  todoDiv.appendChild(removeButton);
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.classList.add("remove-button");
+    todoDiv.appendChild(removeButton);
 
-  toDoList.appendChild(todoDiv);
+    toDoList.appendChild(todoDiv);
 
-  toDoInput.value = "";
+    toDoInput.value = "";
+  }
 }
 
 function handleToDoAction(event) {
@@ -41,4 +46,19 @@ function handleToDoAction(event) {
   if (toDo.classList.contains("mark-button")) {
     toDo.parentElement.firstElementChild.classList.toggle("marked");
   }
+}
+
+function showErrorMessage() {
+  const errorDiv = document.createElement("div");
+  errorDiv.classList.add("error-container");
+
+  const errorParagraph = document.createElement("p");
+  errorParagraph.classList.add("error-message");
+  errorParagraph.innerText = "Input field must not be empty";
+  errorDiv.appendChild(errorParagraph);
+
+  const form = document.querySelector(".form");
+  form.insertAdjacentElement("beforebegin", errorDiv);
+
+  setTimeout(() => document.querySelector(".error-container").remove(), 1000);
 }
